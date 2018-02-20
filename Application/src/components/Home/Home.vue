0fd76c0d58545fc147824aad616aa3d9369a0034
Alex Schouls <alex.schouls@valtech.nl>
2018-02-20 15:33:46 +0100

    Implemented small Vuex Store example

--- Application/src/components/Home/Home.vue
+++ Application/src/components/Home/Home.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="home">
       <h1>HBoard</h1>
+      <h2>Count: {{ count }}</h2>
+      <input type="button" @click="increment"/>
   </div>
 </template>
 
 <script>
+import store from '../../store/store.js'
 export default {
-    name: 'home'
+    name: 'home',
+    computed: {
+        count() {
+            return store.state.count
+        }
+    },
+    methods: {
+        increment() {
+            store.commit('increment')
+        }
+    }
 }
 </script>
 
