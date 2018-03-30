099fc7e3eeb8b77982eee5d717ab3c0af0eb6617
Alex Schouls <alex.schouls@valtech.nl>
2018-03-30 13:57:50 +0200

    Cleaned up Javascript and added some static files

--- Application/src/components/Maps/Maps.vue
+++ Application/src/components/Maps/Maps.vue
@@ -1,39 +1,45 @@
 <template>
-  <div>
-      {{getTimeToWork()}}
-      {{getTimeToHome()}}
-      Time to work: {{timeToWork}}
-      <br />
-      Time to home: {{timeToHome}}
-  </div>
+    <div class="grid">
+        <div>
+          <h2>Travel time by car</h2>
+            <div>
+                <p>Time from home to work: {{ timeToWork }}</p>
+            </div>
+            <div>
+                <p>Time from work to home: {{ timeToHome }}</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import store from '../../store/store.js';
-import MapsService from '../../services/MapsService';
+import store from "../../store/store.js";
+import MapsService from "../../services/MapsService";
+
+let mapsService = new MapsService();
+let home = "IJsselsteen 47, Wijk bij Duurstede";
+let work = "Entrada 705, Amsterdam";
+
+function getTravelTime(origin, destination) {
+  return mapsService.getTravelTime(origin, destination).then(data => {return data});
+}
 
 export default {
-    name: 'maps',
-    data: function() {
-        return { 
-            timeToWork: "",
-            timeToHome: ""
-        }
-    },
-    methods: {
-        getTimeToWork: function() {
-            let service = new MapsService();
-            service.getTravelTime("IJsselsteen 47, Wijk bij Duurstede", "Entrada 705, Amsterdam").then(data => {
-                this.timeToWork = data;
-            });
-        },
-        getTimeToHome: function() {
-           let service = new MapsService();
-            service.getTravelTime("Entrada 705, Amsterdam", "IJsselsteen 47, Wijk bij Duurstede").then(data => {
-                this.timeToHome = data;
-            });
-        }
+  name: "maps",
+  data: function() {
+    return {
+      timeToWork: "",
+      timeToHome: ""
     }
+  },
+  mounted: function() {
+    getTravelTime(home, work).then(data =>{
+      this.timeToWork = data;
+    })
+    getTravelTime(work, home).then(data =>{
+      this.timeToHome = data;
+    })
+  }
 }
 </script>
 
